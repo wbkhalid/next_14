128900300f9edfd39c130a7f75c433f37ae31b7b
Waleed Bin Khalid <waleedbinkhalidch@gmail.com>
2024-02-29 15:59:00 +0500

    home about and conact page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import Image from "next/image";
+import styles from "./home.module.css";
 
-const page = () => {
+const Home = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+        <p className={styles.desc}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
+          blanditiis adipisci minima reiciendis a autem assumenda dolore.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg}/>
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg}/>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default Home;
